f63fffbebd284e7ff0b1f8ed4aba486c9d8c3416
Neto Sousa <netosousa29@yahoo.com>
2022-07-01 11:43:32 -0300

    Implementando API de contexto

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import SignIn from './pages/SignIn';
 //import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
     <>
-        <SignIn />
+        <AuthContext.Provider value={{ name: 'Neto' }}>
+            <SignIn />
+        </AuthContext.Provider> 
+         
         <GlobalStyle />
     </>
 );
